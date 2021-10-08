6092d0cadf7109508b9bb7acc8d5e3a299c705e2
guosong lu <1054595718@qq.com>
2021-10-08 23:02:57 +0800

    2021.10.8

--- _java/spring/spring-security/spring-security-custom-username-password3/src/main/java/cn/com/lgs/config/SecurityConfig.java
+++ _java/spring/spring-security/spring-security-custom-username-password3/src/main/java/cn/com/lgs/config/SecurityConfig.java
@@ -1,11 +1,35 @@
 package cn.com.lgs.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author luguosong
  * @date 2021/10/8 18:18
  */
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
 
+    /**
+     * 指定UserDetailsService实现类
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(password());
+    }
+
+    @Bean
+    PasswordEncoder password(){
+        return new BCryptPasswordEncoder();
+    }
 }

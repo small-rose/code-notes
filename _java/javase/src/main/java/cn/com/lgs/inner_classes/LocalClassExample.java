d4f7e92a659c6e007f19b423e6684ae856f03d90
guosong lu <1054595718@qq.com>
2022-01-03 23:26:25 +0800

    代码提交

---
+++ _java/javase/src/main/java/cn/com/lgs/inner_classes/LocalClassExample.java
@@ -0,0 +1,16 @@
+package cn.com.lgs.inner_classes;
+
+/**
+ * 局部类
+ *
+ * @author 10545
+ * @date 2022/1/3 20:37
+ */
+public class LocalClassExample {
+    static String regularExpression = "[^0-9]";
+
+    public static void validatePhoneNumber(String phoneNumber1, String phoneNumber2) {
+        final int numberLength = 10;
+
+    }
+}

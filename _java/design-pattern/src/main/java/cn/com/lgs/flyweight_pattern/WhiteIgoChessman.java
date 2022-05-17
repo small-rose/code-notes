54c14f4497bbc5df7c0c3d791abca23201eafdf0
guosong lu <1054595718@qq.com>
2022-05-18 00:24:58 +0800

    代码提交

---
+++ _java/design-pattern/src/main/java/cn/com/lgs/flyweight_pattern/WhiteIgoChessman.java
@@ -0,0 +1,14 @@
+package cn.com.lgs.flyweight_pattern;
+
+/**
+ * 白色棋子类，充当具体享元类
+ *
+ * @author 10545
+ * @date 2022/5/17 21:49
+ */
+public class WhiteIgoChessman extends IgoChessman{
+    @Override
+    public String getColor() {
+        return "白色";
+    }
+}

797b95db5e40e310e74d251e1f50feaef4932383
luguosong <1054595718@qq.com>
2022-07-06 00:50:06 +0800

    代码提交

--- _java/java_database/_01_jdbc/src/main/java/com/luguosong/_04_prepare_statement/PrepareStatementDemo.java
+++ _java/java_database/_01_jdbc/src/main/java/com/luguosong/_04_prepare_statement/PrepareStatementDemo.java
@@ -10,20 +10,32 @@ public class PrepareStatementDemo {
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
+        ResultSet resultSet = null;
         try {
             connection = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/java_database",
                     "root",
                     "12345678");
-
+            //静态sql
             statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM user WHERE name='张三' AND password='a' OR 'a' = 'a'");
-            System.out.println("静态sql查询结果："+resultSet.next());
-
-            connection.prepareStatement("SELECT * FROM user WHERE name=? AND password=?");
+            resultSet = statement.executeQuery("SELECT * FROM user WHERE name='张三' AND password='a' OR 'a' = 'a'");
+            System.out.println("静态sql查询结果：" + resultSet.next());
+            //通过preparedStatement查询
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user WHERE name=? AND password=?");
+            preparedStatement.setString(1, "张三");
+            preparedStatement.setString(2, "'a' OR 'a' = 'a'");
+            ResultSet resultSet1 = preparedStatement.executeQuery();
+            System.out.println("动态sql查询结果：" + resultSet1.next());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             if (statement != null) {
                 try {
                     statement.close();
